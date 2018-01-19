a254ca2e8ff544ae4e5ddf29ca9a576f57152ac5
PresentKim <qjrmckd@gmail.com>
2018-01-20 08:17:21 +0900

    Add command classes

---
+++ src/presentkim/humanoid/command/PoolCommand.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace presentkim\humanoid\command;
+
+
+use pocketmine\command\{
+  Command, CommandExecutor, CommandSender, PluginCommand
+};
+use presentkim\humanoid\util\Translation;
+use presentkim\humanoid\HumanoidMain as Plugin;
+
+class PoolCommand extends PluginCommand implements CommandExecutor{
+
+    /** @var SubCommand[] */
+    protected $subCommands = [];
+
+    /** @var \ReflectionProperty */
+    private $property = null;
+
+    /** @var string */
+    public $uname;
+
+    /**
+     * @param Plugin       $owner
+     * @param string       $name
+     * @param SubCommand[] $subCommands
+     */
+    public function __construct(Plugin $owner, string $name, SubCommand ...$subCommands){
+        parent::__construct($name, $owner);
+        $this->setExecutor($this);
+
+        $reflection = new \ReflectionClass(Command::class);
+        $this->property = $reflection->getProperty('name');
+        $this->property->setAccessible(true);
+
+        $this->uname = $name;
+        $this->setPermission("{$name}.cmd");
+        $this->updateTranslation();
+
+        $this->subCommands = $subCommands;
+    }
+
+    /**
+     * @param CommandSender $sender
+     * @param Command       $command
+     * @param string        $label
+     * @param string[]      $args
+     *
+     * @return bool
+     */
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+        if (!isset($args[0])) {
+            return false;
+        } else {
+            $label = array_shift($args);
+            foreach ($this->subCommands as $key => $value) {
+                if ($value->checkLabel($label)) {
+                    $value->execute($sender, $args);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    /** @return SubCommand[] */
+    public function getSubCommands() : array{
+        return $this->subCommands;
+    }
+
+    /** @param SubCommand[] $subCommands */
+    public function setSubCommands(SubCommand ...$subCommands){
+        $this->subCommands = $subCommands;
+    }
+
+    /** @param SubCommand::class $subCommandClass */
+    public function createSubCommand($subCommandClass){
+        $this->subCommands[] = new $subCommandClass($this);
+    }
+
+    public function updateTranslation(){
+        $this->property->setValue($this, Translation::translate("command-{$this->uname}"));
+        $this->description = Translation::translate("command-{$this->uname}@description");
+        $this->usageMessage = Translation::translate("command-{$this->uname}@usage");
+        $aliases = Translation::getArray("command-{$this->uname}@aliases");
+        if (is_array($aliases)) {
+            $this->setAliases($aliases);
+        }
+    }
+
+    public function updateSudCommandTranslation(){
+        foreach ($this->subCommands as $key => $value) {
+            $value->updateTranslation();
+        }
+    }
+}
---
+++ src/presentkim/humanoid/command/SubCommand.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace presentkim\humanoid\command;
+
+use pocketmine\command\CommandSender;
+use presentkim\humanoid\{
+  HumanoidMain as Plugin, util\Translation
+};
+use function presentkim\humanoid\util\in_arrayi;
+
+abstract class SubCommand{
+
+    /** @var PoolCommand */
+    protected $owner;
+
+    /** @var Plugin */
+    protected $plugin;
+
+    /** @var string */
+    protected $strId;
+
+    /** @var string */
+    protected $permission;
+
+    /** @var string */
+    protected $label;
+
+    /** @var string[] */
+    protected $aliases;
+
+    /** @var string */
+    protected $usage;
+
+    /**
+     * SubCommand constructor.
+     *
+     * @param PoolCommand $owner
+     * @param string      $label
+     */
+    public function __construct(PoolCommand $owner, string $label){
+        $this->owner = $owner;
+        $this->plugin = $owner->getPlugin();
+
+        $this->strId = "command-{$owner->uname}-{$label}";
+        $this->permission = "{$owner->uname}.cmd.{$label}";
+
+        $this->updateTranslation();
+    }
+
+    /**
+     * @param CommandSender $sender
+     * @param String[]      $args
+     */
+    public function execute(CommandSender $sender, array $args){
+        if (!$this->checkPermission($sender)) {
+            $sender->sendMessage(Plugin::$prefix . Translation::translate('command-generic-failure@permission'));
+        } elseif (!$this->onCommand($sender, $args)) {
+            $sender->sendMessage(Plugin::$prefix . $this->usage);
+        }
+    }
+
+    /**
+     * @param CommandSender $sender
+     * @param String[]      $args
+     *
+     * @return bool
+     */
+    abstract public function onCommand(CommandSender $sender, array $args);
+
+    /**
+     * @param CommandSender $target
+     *
+     * @return bool
+     */
+    public function checkPermission(CommandSender $target){
+        if ($this->permission === null) {
+            return true;
+        } else {
+            return $target->hasPermission($this->permission);
+        }
+    }
+
+    /**
+     * @param string   $tag
+     * @param string[] $params
+     *
+     * @return string
+     */
+    public function translate(string $tag, string ...$params){
+        return Translation::translate("{$this->strId}@{$tag}", ...$params);
+    }
+
+    /**
+     * @param string $label
+     *
+     * @return bool
+     */
+    public function checkLabel(string $label){
+        return strcasecmp($label, $this->label) === 0 || $this->aliases && in_arrayi($label, $this->aliases);
+    }
+
+    public function updateTranslation(){
+        $this->label = Translation::translate($this->strId);
+        $this->aliases = Translation::getArray("{$this->strId}@aliases");
+        $this->usage = $this->translate('usage');
+    }
+}
