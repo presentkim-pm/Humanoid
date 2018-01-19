7c2ec6de9a58bc4a915a8bb6b023099dc0303261
PresentKim <qjrmckd@gmail.com>
2018-01-20 07:36:39 +0900

    Call player click humanoid event at InventoryTransactionPacket
    with DataPacketReceiveEvent cancel

--- src/presentkim/humanoid/listener/PlayerEventListener.php
+++ src/presentkim/humanoid/listener/PlayerEventListener.php
@@ -5,6 +5,10 @@ namespace presentkim\humanoid\listener;
 use pocketmine\event\{
   Listener, server\DataPacketReceiveEvent
 };
+use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
+use pocketmine\Server;
+use presentkim\humanoid\entity\Humanoid;
+use presentkim\humanoid\event\PlayerClickHumanoidEvent;
 use presentkim\humanoid\HumanoidMain as Plugin;
 
 class PlayerEventListener implements Listener{
@@ -18,6 +22,16 @@ class PlayerEventListener implements Listener{
 
     /** @param DataPacketReceiveEvent $event */
     public function onDataPacketReceiveEvent(DataPacketReceiveEvent $event){
-        
+        $player = $event->getPlayer();
+        $pk = $event->getPacket();
+        if ($pk instanceof InventoryTransactionPacket) {
+            if ($pk->transactionType === InventoryTransactionPacket::TYPE_USE_ITEM_ON_ENTITY) {
+                $target = $player->level->getEntity($pk->trData->entityRuntimeId);
+                if ($target instanceof Humanoid) {
+                    $event->setCancelled(true);
+                    Server::getInstance()->getPluginManager()->callEvent(new PlayerClickHumanoidEvent($player, $target, $pk->trData->actionType));
+                }
+            }
+        }
     }
 }
