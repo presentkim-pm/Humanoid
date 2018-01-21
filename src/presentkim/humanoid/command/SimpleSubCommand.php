51ae2c71276b63a28ed846bb73772df8fb5e5556
PresentKim <qjrmckd@gmail.com>
2018-01-21 11:36:39 +0900

    Add simple sub command

---
+++ src/presentkim/humanoid/command/SimpleSubCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace presentkim\humanoid\command;
+
+use pocketmine\command\CommandSender;
+use presentkim\humanoid\{
+  HumanoidMain as Plugin, util\Translation
+};
+use function presentkim\humanoid\util\in_arrayi;
+
+abstract class SimpleSubCommand{
+
+    /** @var string */
+    protected $label;
+
+    /** @var string[] */
+    protected $aliases;
+
+    /** @var string */
+    protected $usage;
+
+    /**
+     * SubCommand constructor.
+     *
+     * @param string   $label
+     * @param string[] $aliases
+     */
+    public function __construct(string $label, array $aliases){
+        $this->label = $label;
+        $this->aliases = $aliases;
+    }
+
+    /**
+     * @param CommandSender $sender
+     * @param String[]      $args
+     *
+     * @return bool
+     */
+    abstract public function onCommand(CommandSender $sender, array $args);
+
+    /**
+     * @param string $label
+     *
+     * @return bool
+     */
+    public function checkLabel(string $label){
+        return strcasecmp($label, $this->label) === 0 || $this->aliases && in_arrayi($label, $this->aliases);
+    }
+
+    /** @return string */
+    public function getLabel(){
+        return $this->label;
+    }
+
+    /** @param string $label */
+    public function setLabel(string $label){
+        $this->label = $label;
+    }
+
+    /** @return string[] */
+    public function getAliases(){
+        return $this->aliases;
+    }
+
+    /** @param string[] $aliases */
+    public function setAliases(array $aliases){
+        $this->aliases = $aliases;
+    }
+
+}
