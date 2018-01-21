1067d45617d98876960febc8e20d8cf5b263751e
PresentKim <qjrmckd@gmail.com>
2018-01-21 10:42:15 +0900

    Handle player event to player tasks

--- src/presentkim/humanoid/listener/PlayerEventListener.php
+++ src/presentkim/humanoid/listener/PlayerEventListener.php
@@ -7,6 +7,9 @@ use pocketmine\event\{
 };
 use presentkim\humanoid\event\PlayerClickHumanoidEvent;
 use presentkim\humanoid\HumanoidMain as Plugin;
+use presentkim\humanoid\task\{
+  PlayerTask, HumanoidSetTask
+};
 
 class PlayerEventListener implements Listener{
 
@@ -19,11 +22,17 @@ class PlayerEventListener implements Listener{
 
     /** @param PlayerClickHumanoidEvent $event */
     public function onPlayerClickHumanoidEvent(PlayerClickHumanoidEvent $event){
-
+        $task = PlayerTask::getTask($player = $event->getPlayer());
+        if ($task instanceof HumanoidSetTask) {
+            $task->onClickHumanoid($event);
+        }
     }
 
     /** @param PlayerInteractEvent $event */
     public function onPlayerInteractEvent(PlayerInteractEvent $event){
-
+        $task = PlayerTask::getTask($player = $event->getPlayer());
+        if ($task instanceof PlayerTask) {
+            $task->onInteract($event);
+        }
     }
 }
