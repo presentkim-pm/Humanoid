5fb3325c532d432e2633bef3050beacfa88f071b
PresentKim <qjrmckd@gmail.com>
2018-02-08 02:43:35 +0900

    StealHumanoidSkinAct : sendSkin to player

--- src/presentkim/humanoid/act/child/StealHumanoidSkinAct.php
+++ src/presentkim/humanoid/act/child/StealHumanoidSkinAct.php
@@ -14,6 +14,7 @@ class StealHumanoidSkinAct extends PlayerAct implements ClickHumanoidAct{
     /** @param PlayerClickHumanoidEvent $event */
     public function onClickHumanoid(PlayerClickHumanoidEvent $event) : void{
         $this->player->setSkin($event->getHumanoid()->getSkin());
+        $this->player->sendSkin();
 
         $this->player->sendMessage(Plugin::$prefix . Translation::translate('humanoid-skinsteal@success'));
 
